a3d270028d15b23e305401327b6c66958294f969
Gohand <Andy.Notfound@gmail.com>
2025-05-18 22:36:40 +0700

    fix(api): role based, rename endpoint, and refac

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TestController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ComboController;
@@ -9,75 +8,157 @@ use App\Http\Controllers\PackageController;
 use App\Http\Controllers\TravelTransactionController;
 use App\Http\Controllers\UserController;
 
-// Private routes
-Route::group(['middleware' => ['auth:api', 'refresh_token']], function () {
-    // Authentication
-    Route::prefix('/auth')->group(function () {
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::post('/logout', [AuthController::class, 'logout']);
-    });
+/* TODO: 
+    1. Add middleware for role-based access control
+    2. Ensure role-based middleware working as expected
+*/
 
-    // User 
-    Route::prefix('/user')->group(function () {
-        Route::post('/update', [UserController::class, 'update']);
-        Route::post('/active', [UserController::class, 'toggleUserAccountStatus']);
-    });
+// Authentication
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('register', [AuthController::class, 'register'])->name('auth.register');
 
-    Route::prefix('/packages')->group(function () {
-        Route::get('/list', [PackageController::class, 'list']);
-        Route::get('{Oid}', [PackageController::class, 'show']);
+    Route::middleware(['auth:api', 'refresh_token'])->group(function () {
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
+
+// Travel Packages Management
+Route::prefix('/packages')->group(function () {
+    Route::get('/list', [PackageController::class, 'list']);
+    Route::get('/favorites', [PackageController::class, 'favorites']);
+    Route::get('/seasonal', [PackageController::class, 'seasonal']);
+    Route::get('/custom', [PackageController::class, 'custom']);
+    Route::get('/mustsee', [PackageController::class, 'mustsee']);
+
+    Route::get('/{Oid}', [PackageController::class, 'show']);
+
+    Route::middleware(['auth:api', 'role:admin', 'refresh_token'])->group(function () {
         Route::delete('{Oid}', [PackageController::class, 'delete']);
     });
+});
 
-    Route::prefix('/cart')->group(function () {
-        Route::post('/create', [CartController::class, 'create']);
-        Route::post('/Update-Payment', [CartController::class, 'updatePayment']);
-        Route::post('/Create-Payment/{Oid}', [CartController::class, 'createPayment']);
+// User Management
+Route::prefix('/user')->group(function () {
+    Route::middleware(['auth:api', 'role:admin', 'refresh_token'])->group(function () {
+        Route::post('/update', [UserController::class, 'update']);
+        Route::post('/active', [UserController::class, 'toggleUserAccountStatus']);
+        Route::post('/assign-role/{userId}', [UserController::class, 'assignRoleToUser']);
     });
+});
 
-    Route::prefix('/admin')->group(function () {
-        Route::prefix('/travelTransaction')->group(function () {
+// Cart Management
+Route::prefix('/cart')->group(function () {
+    Route::middleware(['auth:api', 'refresh_token'])->group(function () {
+        Route::post('/create', [CartController::class, 'create']);
+        Route::post('/update-payment', [CartController::class, 'updatePayment']);
+        Route::post('/create-payment/{Oid}', [CartController::class, 'createPayment']);
+        /* TODO:
+            Ubah callback nya di xendit lagi berarti?
+            Karena url nya dari /callback-payment ke cart/callback-payment
+        */
+        Route::post('/callback-payment', [CartController::class, 'updatePayment']);
+    });
+});
+
+// Travel Transaction Management
+Route::prefix('/travel-transaction')->group(function () {
+    Route::middleware(['auth:api', 'role:admin', 'refresh_token'])->group(function () {
+        Route::prefix('/admin')->group(function () {
             Route::get('/list', [TravelTransactionController::class, 'list']);
             Route::get('{Oid}', [TravelTransactionController::class, 'show']);
             Route::post('{Oid}', [TravelTransactionController::class, 'save']);
         });
     });
+});
+
+// Itineraries Management
+Route::prefix('/itineraries')->group(function () {
+    Route::get('/combosource', [ComboController::class, 'itineraries']);
+});
+
+/*-------- Private routes --------*/
+
+Route::group(['middleware' => ['auth:api', 'refresh_token']], function () {
+    /*  Authentication
+        Route::prefix('/auth')->group(function () {
+            Route::post('/refresh', [AuthController::class, 'refresh']);
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
+    */
+
+    /*  User 
+        Route::prefix('/user')->group(function () {
+            Route::post('/update', [UserController::class, 'update']);
+            Route::post('/active', [UserController::class, 'toggleUserAccountStatus']);
+            // Route::post('{userId}/assign-role', [UserController::class, 'assignRoleToUser']);
+        });
+    */
+
+    /*  
+        Route::prefix('/packages')->group(function () {
+                // Why is viewing the package list and detail restricted to authenticated users?
+                // Route::get('/list', [PackageController::class, 'list']);
+                // Route::get('{Oid}', [PackageController::class, 'show']);
+            Route::delete('{Oid}', [PackageController::class, 'delete']);
+        });
+    */
 
     /*
-        Unused Role based middleware?
-        Route::middleware(['role:owner'])->group(function () {
+        Route::prefix('/cart')->group(function () {
+            Route::post('/create', [CartController::class, 'create']);
+            Route::post('/Update-Payment', [CartController::class, 'updatePayment']);
+            Route::post('/Create-Payment/{Oid}', [CartController::class, 'createPayment']);
         });
-        Route::middleware(['role:admin'])->group(function () {
+    */
+
+    /*  Travel Transaction
+        Route::prefix('/admin')->group(function () {
+            Route::prefix('/travelTransaction')->group(function () {
+                Route::get('/list', [TravelTransactionController::class, 'list']);
+                Route::get('{Oid}', [TravelTransactionController::class, 'show']);
+                Route::post('{Oid}', [TravelTransactionController::class, 'save']);
+            });
         });
-        Route::middleware(['role:client'])->group(function () {
-        });
+    */
+
+    /*  Unused Role based middleware?
+        Route::middleware(['role:owner'])->group(function () {});
+        Route::middleware(['role:admin'])->group(function () {});
+        Route::middleware(['role:client'])->group(function () {});
     */
 });
 
-// Public routes
+/*-------- Public routes --------*/
 
-// Authentication
-Route::prefix('/auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-});
+/*  Authentication
+    Route::prefix('/auth')->group(function () {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);
+    });
+*/
 
-// Homepage
-Route::prefix('/homepage')->group(function () {
-    Route::get('/favorites', [PackageController::class, 'favorites']);
-    Route::get('/seasonal', [PackageController::class, 'seasonal']);
-    Route::get('/custom', [PackageController::class, 'custom']);
-    Route::get('/mustsee', [PackageController::class, 'mustsee']);
-});
+/*  Travel Packages
+    Route::prefix('/packages')->group(function () {
+        Route::get('/list', [PackageController::class, 'list']);
+        Route::get('/{Oid}', [PackageController::class, 'show']);
+    });
+*/
+
+/*  Homepage Travel Packages List
+    Route::prefix('/homepage')->group(function () {
+        Route::get('/favorites', [PackageController::class, 'favorites']);
+        Route::get('/seasonal', [PackageController::class, 'seasonal']);
+        Route::get('/custom', [PackageController::class, 'custom']);
+        Route::get('/mustsee', [PackageController::class, 'mustsee']);
+    });
+*/
 
 Route::post('/Callback-Payment', [CartController::class, 'updatePayment']);
 
-Route::prefix('/combosource')->group(function () {
-    Route::get('/itineraries', [ComboController::class, 'itineraries']);
-});
-
-/* 
-    Assign role to user
-    use App\Http\Controllers\UserController;
-    Route::post('/users/{userId}/assign-role', [UserController::class, 'assignRoleToUser']);
+/*  Itineraries
+    Route::prefix('/combosource')->group(function () {
+        Route::get('/itineraries', [ComboController::class, 'itineraries']);
+    });
 */
