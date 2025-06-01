33c738a364cf9ac72152bcdd4073fd994ea64a57
Max <maximusflorenzo@gmail.com>
2025-06-02 00:15:50 +0700

    Price On Travel Transaction

--- app/Models/TravelTransaction.php
+++ app/Models/TravelTransaction.php
@@ -21,14 +21,30 @@ class TravelTransaction extends Model
         'CreatedAt',
         'Code',
         'Description',
+        'Price',
         'Packages',
     ];
 
-    public function creator() { return $this->belongsTo(User::class, 'CreateBy', 'user_id'); }
-    public function PackagesObj() { return $this->belongsTo(Packages::class, 'Packages', 'Oid');  }
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'CreateBy', 'user_id');
+    }
+    public function PackagesObj()
+    {
+        return $this->belongsTo(Packages::class, 'Packages', 'Oid');
+    }
     // public function TrvTransactionDetailObj() { return $this->belongsTo(TravelTransactionDetail::class, 'TravelTransaction', 'Oid');  }
-    public function details(){ return $this->hasMany(TravelTransactionDetail::class, 'TravelTransaction', 'Oid'); }
-    public function packages() { return $this->hasMany(\App\Models\Packages::class, 'Oid', 'Packages'); }
+    public function details()
+    {
+        return $this->hasMany(TravelTransactionDetail::class, 'TravelTransaction', 'Oid');
+    }
+    public function packages()
+    {
+        return $this->hasMany(\App\Models\Packages::class, 'Oid', 'Packages');
+    }
 
-    public function package(){ return $this->belongsTo(Packages::class, 'Packages', 'Oid'); }
+    public function package()
+    {
+        return $this->belongsTo(Packages::class, 'Packages', 'Oid');
+    }
 }
