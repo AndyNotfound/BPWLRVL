7aa95238972216610b536a05d926cac79be0f89f
Gohand <Andy.Notfound@gmail.com>
2025-06-01 18:25:45 +0700

    refac(travel-transaction): change the data on get

--- app/Models/TravelTransaction.php
+++ app/Models/TravelTransaction.php
@@ -27,6 +27,8 @@ class TravelTransaction extends Model
     public function creator() { return $this->belongsTo(User::class, 'CreateBy', 'user_id'); }
     public function PackagesObj() { return $this->belongsTo(Packages::class, 'Packages', 'Oid');  }
     // public function TrvTransactionDetailObj() { return $this->belongsTo(TravelTransactionDetail::class, 'TravelTransaction', 'Oid');  }
-    public function details() { return $this->hasMany(\App\Models\TravelTransactionDetail::class, 'TravelTransaction', 'Oid'); }
+    public function details(){ return $this->hasMany(TravelTransactionDetail::class, 'TravelTransaction', 'Oid'); }
     public function packages() { return $this->hasMany(\App\Models\Packages::class, 'Oid', 'Packages'); }
+
+    public function package(){ return $this->belongsTo(Packages::class, 'Packages', 'Oid'); }
 }
